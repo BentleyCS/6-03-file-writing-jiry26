8874f20130de68109afa4561cf78f44e7e997ede
jiry26 <jiry26@bentleyschool.org>
2026-03-02 09:04:16 -0800

    Add files via upload

--- CSP_6_03_Writing_to_files.py
+++ CSP_6_03_Writing_to_files.py
@@ -1,26 +1,47 @@
 import random
 
+
 #You need to create your own test file for this assignment.
 #Because we are dealing with both reading and writing to files. Your test file will be more complicated than it has been.
 
 def writeFile(inputList, fileName):
-    #Creates a file of the given name and adds each value from the list to said file with each line being an index from the list.
-    pass
+    with open(fileName, "w") as file:
+        for n in inputList:
+            file.write(f"{n}\n")
+    return fileName
+#writeFile([1,"hello",49,"haha"],"random file")
 
 
-def sortNames(fileName, targetFile):
+def sortNames(sourceName, targetName):
     #Modify the below function such that it takes in source file and a target file.
     #Sort all of the values from the source file and write them to the target file
     #I recommend using .sort() for this. You do not need to write the sorting algorithm yourself.
-    pass
+    randomList = []
+    with open(sourceName, "r") as sourceFile:
+        with open(targetName, "w") as targetFile:
+            for line in sourceFile:
+                randomList.append(line)
+            randomList.sort()
+            for n in randomList:
+                targetFile.write(n)
+#sortNames("fileName","targetName")
 
-sortNames("names.txt","namesNew.txt")
-
-
+def get_average(numbers):
+    return sum(numbers) / len(numbers) if numbers else 0
 
 def highScore( newScore: int):
+    scoresList = []
+    with open("scores.txt", "a") as file:
+        file.write(f"{newScore}\n")
+    with open("scores.txt", "r") as file:
+        for line in file:
+            scoresList.append(int(line))
+    avg = get_average(scoresList)
+    return(avg)
+
     #Modify the function such that it adds a new score to the file scores.txt
     #Then return the average score from all of the scores in scores.txt
     pass
-highScore(random.randint(1,100))
+print(highScore(30))
+#highScore(random.randint(1,100))
 
